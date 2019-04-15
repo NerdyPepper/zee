f129713f7deca06bdfc7d4ab97169725ebabe848
NerdyPepper <akshayoppiliappan@gmail.com>
2019-04-15 12:17:05 +0530

    new impls for polar

--- src/polar/mod.rs
+++ src/polar/mod.rs
@@ -1,3 +1,23 @@
-#[derive(Debug)]
-pub struct ZPolar(pub f64, pub f64);
+use crate::rect::ZRect;
+
+#[derive(Debug, Clone)]
+pub struct ZPolar (pub f64, pub f64);
+
+impl ZPolar {
+    pub fn new(x: f64, y: f64) -> Self {
+        ZPolar(x, y)
+    }
+    pub fn to_rect(&self) -> ZRect {
+        ZRect(
+            self.0 * self.1.cos(),
+            self.0 * self.1.sin()
+        )
+    }
+}
+
+impl From<ZPolar> for (f64, f64) {
+    fn from(x: ZPolar) -> Self {
+        (x.0, x.1)
+    }
+}
 
